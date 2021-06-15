4f2bcd7915632995d651859ca66ef39e5c7130bf
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2021-06-15 08:45:49 -0400

    init

---
+++ dev/serve.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineComponent } from 'vue';
+import AosVue from '@/aos-vue.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    AosVue
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <aos-vue animation='fade-in' :duration='2000'>
+      <p>Hello World</p>
+    </aos-vue>
+  </div>
+</template>
---
+++ src/aos-vue.vue
@@ -0,0 +1,69 @@
+<script>
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AosVue",
+  mounted() {
+    AOS.init();
+    AOS.refresh();
+  },
+  props: {
+    animation: {
+      type: String,
+      required: true,
+    },
+    offset: {
+      type: String,
+      required: false,
+      default: 0,
+    },
+    once: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    mirror: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    easing: {
+      type: String,
+      required: false,
+      default: "ease-in-out",
+    },
+    duration: {
+      type: Number,
+      required: false,
+      default: 400,
+    },
+    delay: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    anchor: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+});
+</script>
+
+<template>
+  <div
+    :data-aos="animation"
+    :data-aos-duration="duration"
+    :data-aos-easing="easeing"
+    :data-aos-mirror="mirror"
+    :data-aos-once="once"
+    :data-aos-offset="offset"
+    :data-aos-delay="delay"
+    :data-aos-anchor="anchor"
+  >
+    <slot />
+  </div>
+</template>
